2b3032c9c3a29b18b80d7897425acd18ef5b6a63
HOYE0NG <hy8839@naver.com>
2022-12-20 19:19:12 +0900

    [add] 문자열 포맷팅

--- practice.py
+++ practice.py
@@ -22,3 +22,19 @@ print(str.find("a",str.find("a")+1))
 print(str.find("파"))
 #해당 문자의 개수
 print(str.count("is"))
+
+#문자열 포맷팅
+print("a"+"b") #붙여쓰기
+print("a","b") #?
+# % 사용
+print("나는 %d살입니다." %20) #한 개 포맷팅
+print("나는 %d살이고 %s를 좋아합니다." % (20, "사과")) #두 개 이상 포맷팅
+# {} 사용
+print("나는 {}살이고 {}를 좋아합니다." .format(20,"사과"))
+print("나는 {1}살이고 {0}를 좋아합니다.".format("사과",20))
+print("나는 {age}살이고 {fruit}를 좋아합니다.".format(age=20, fruit="사과"))
+
+# f 사용 (~3.6v)
+age = 20
+fruit = "사과"
+print(f"나는 {age}살이고 {fruit}를 좋아해요.")
