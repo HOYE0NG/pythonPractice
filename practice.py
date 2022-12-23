a7088717750b7257111b32f61e4d9620d170d2f1
HOYE0NG <hy8839@naver.com>
2022-12-23 16:52:26 +0900

    [add] 파일 입출력

--- practice.py
+++ practice.py
@@ -376,3 +376,44 @@ print("{0:+,}".format(1000000000))
 print("{0:^<+30,}".format(1000000000)) 
 #소수점 출력 + 특정 자리수
 print("{0:.3f}".format(5/3))
+
+
+#파일 입출력
+# score_file = open("score.txt","w",encoding="utf8")
+# print("수학 : 0", file=score_file)
+# print("영어 : 0", file=score_file)
+# score_file.close()
+
+# score_file = open("score.txt", "a", encoding="utf8") #a:이어붙여서쓰기
+# score_file.write("과학 : 80") #줄바꿈 없음
+# score_file.write("\n코딩 : 100")
+
+#읽기
+# score_file = open("score.txt", "r", encoding="utf8")
+# print(score_file.read())
+# score_file.close()
+
+#한줄씩 읽기
+# score_file = open("score.txt", "r", encoding="utf8")
+# print(score_file.readline(), end="") # 줄별로 읽기, 한줄 읽고나서 커서는 다음줄로 이동
+# print(score_file.readline(), end="")
+# print(score_file.readline(), end="")
+# print(score_file.readline(), end="")
+# score_file.close()
+
+# 몇줄인지 모를때 파일 다 읽기
+# score_file = open("score.txt","r",encoding="utf8")
+# while True:
+#     line = score_file.readline()
+#     if not line: #반환값 부울값
+#         break
+#     print(line,end="")
+# score_file.close()
+
+#list에 읽기
+score_file = open("score.txt", "r", encoding="utf8")
+lines = score_file.readlines() #list 형태로 저장
+print
+for line in lines:
+    print(line, end="")
+score_file.close()
